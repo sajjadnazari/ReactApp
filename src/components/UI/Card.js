4d5bfcb9202b7ac77a8583c75dc424716fd64aaf
Sajjad Nazari <sajjad69nazari@gmail.com>
2022-08-21 17:52:31 +0430

    delete and re new project

--- src/components/UI/Card.js
+++
@@ -1,8 +0,0 @@
-import './Card.css'
-
-const Card = (props) => {
-    const cardClasses = 'card ' + props.className;
-    return (<div className={cardClasses}>{props.children}</div>);
-}
-
-export default Card;
